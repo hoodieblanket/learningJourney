2ebf583650e9ee527374b71581685acb7ce7d982
hoodieblanket <vanjo217@gmail.com>
2019-02-20 22:56:26 +1100

    Added in Hangman game and text file with words to grab from

--- Programs/Python3/(Game) Hangman/(Game) Hangman.py
+++ Programs/Python3/(Game) Hangman/(Game) Hangman.py
@@ -7,9 +7,7 @@ WORDLIST_FILENAME = "words.txt"
 def loadWords():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-
-    Depending on the size of the word list, this function may
-    take a while to finish.
+\
     """
     print("Loading word list from file...")
     # inFile: file
@@ -30,12 +28,6 @@ def chooseWord(wordlist):
     """
     return random.choice(wordlist)
 
-
-# end of helper code
-# -----------------------------------
-
-# Load the list of words into the variable wordlist
-# so that it can be accessed from anywhere in the program
 wordlist = loadWords()
 
 
@@ -90,19 +82,6 @@ def hangman(secretWord):
 
     Starts up an interactive game of Hangman.
 
-    * At the start of the game, let the user know how many
-      letters the secretWord contains.
-
-    * Ask the user to supply one guess (i.e. letter) per round.
-
-    * The user should receive feedback immediately after each guess
-      about whether their guess appears in the computers word.
-
-    * After each round, you should also display to the user the
-      partially guessed word so far, as well as letters that the
-      user has not yet guessed.
-
-    Follows the other limitations detailed in the problem write-up.
     '''
     print("-----------------------")
     print("Welcome to the game Hangman! \
